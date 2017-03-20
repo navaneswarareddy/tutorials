4e4d2769c0e3b0182f025cd5f8ea37158dfdf7fd
naveen <k.navaneswarareddy@gmail.com>
2017-03-20 21:19:38 +0530

    first commit

---
+++ C++/Burrito.cpp
@@ -0,0 +1,11 @@
+// 15_cppBeginners
+
+#include "Burrito.h" // insert the header file
+#include <iostream>
+
+using namespace std;
+
+Burrito::Burrito() // constructor
+{
+    cout << "i am a bananan" << endl;
+}
